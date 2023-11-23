695100472e7038e5b8f1bbad92f038ef2ddf31c6
ganesh <ganeshkumar159th@gmail.com>
2023-11-23 13:37:37 +0530

    completed up to row one

---
+++ cvip_netflex_clone/src/App.js
@@ -0,0 +1,46 @@
+
+import './App.css';
+import Row from './components/Row';
+import requests from './Request';
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="app">
+
+     <h1>netflix clone</h1>
+
+
+     
+     <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow />
+
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
+
+
+
+
+
+     
+    </div>
+  );
+}
+
+export default App;
